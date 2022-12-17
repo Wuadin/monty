25202f14ebc83021ecd68f6849b9ae0e6d8a37ae
FineNatura <ansyveronic@gmail.com>
2022-12-17 14:30:43 +0000

    task done

---
+++ bytecodes/comments.m
@@ -0,0 +1,11 @@
+#Every good language comes with the capability of commenting 
+#When the first non-space character of a line is #, 
+#treat this line as a comment (don’t do anything)
+#hello world
+#cisfun
+#montyisfun
+push 10
+push 2
+push 8
+push 5
+pall
